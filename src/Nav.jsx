6b4319e881189d1c7b4bed81696ed348002829f8
msr <mar_r@mail.ru>
2023-04-23 11:29:02 +0300

    burger not working

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,18 @@
 import Logo from './Logo'
 import Burger from './Burger'
 import Menu from './Menu'
+import { useState } from 'react'
 
 function Nav() {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => setVisible(!visible)
+
   return (
     <nav className="main__nav nav">
       <Logo />
-      <Burger />
-      <Menu />
+      <Burger onClick={toggleVisibility} />
+      {visible && <Menu />}
     </nav>
   )
 }
