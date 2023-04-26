68ffb9c150ce16de9e0c4c940fed13f219938a79
msr <mar_r@mail.ru>
2023-04-26 07:18:33 +0300

    в процессе

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import FilterPopup from './FilterPopup'
 
 const FILTER_LIST = [
-  { id: 1, type: 'author', text: 'исполнителю' },
-  { id: 2, type: 'year', text: 'году выпуска' },
-  { id: 3, type: 'genre', text: 'жанру' },
+  { type: 'author', text: 'исполнителю' },
+  { type: 'year', text: 'году выпуска' },
+  { type: 'genre', text: 'жанру' },
 ]
 
 function Filter() {
@@ -12,22 +12,9 @@ function Filter() {
   const [filterBy, setFilterBy] = useState()
   const [popupCoords, setPopupCoords] = useState({ top: 0, left: 0 })
 
-  const getFilterType = (target) => {
-    if (target.classList.contains('button-author')) {
-      return 'author'
-    }
-    if (target.classList.contains('button-year')) {
-      return 'year'
-    }
-    if (target.classList.contains('button-genre')) {
-      return 'genre'
-    }
-    return undefined
-  }
-
   const getPopupCoords = (element) => {
     const left = element.offsetLeft
-    const bottom = element.offsetTop + element.offsetHeight
+    const bottom = element.offsetTop + element.offsetHeight + 10
 
     const popupTop = `${bottom}px`
     const popupLeft = `${left}px`
@@ -38,16 +25,15 @@ function Filter() {
     const { target } = event
     if (!target.classList.contains('filter__button')) return
     setPopupCoords(getPopupCoords(target))
-    const filterType = getFilterType(target)
+    const filterType = target.dataset.type
     setFilterBy(filterType)
     setPopupVisible((prevState) => filterType !== filterBy || !prevState)
   }
 
-  const setActive = (filterType) =>
-    isPopupVisible && filterBy === filterType ? ' _btn-text_active' : ''
+  const setActive = (filterType) => (isPopupVisible && filterBy === filterType ? ' _btn-text_active' : '')
 
-  const filterButtons = FILTER_LIST.map(({ id, type, text }) => (
-    <div className={`filter__button button-${type} _btn-text ${setActive(type)}`} key={id}>
+  const filterButtons = FILTER_LIST.map(({ type, text }) => (
+    <div className={`filter__button button-${type} _btn-text ${setActive(type)}`} key={type} data-type={type}>
       {text}
     </div>
   ))
--- src/components/FilterPopup.jsx
+++ src/components/FilterPopup.jsx
@@ -1,86 +1,107 @@
-// const authors = [
-//   {
-//     id: 0,
-//     author: 'Michael Jackson',
-//   },
-//   {
-//     id: 1,
-//     author: 'Frank Sinatra',
-//   },
-//   {
-//     id: 2,
-//     author: 'Arctic Monkeys',
-//   },
-//   {
-//     id: 3,
-//     author: 'Calvin Harris',
-//   },
-//   {
-//     id: 4,
-//     author: 'Zhu',
-//   },
-// ]
+const authors = [
+  {
+    id: 0,
+    author: 'Michael Jackson',
+  },
+  {
+    id: 1,
+    author: 'Frank Sinatra',
+  },
+  {
+    id: 2,
+    author: 'Arctic Monkeys',
+  },
+  {
+    id: 3,
+    author: 'Calvin Harris',
+  },
+  {
+    id: 4,
+    author: 'Zhu',
+  },
+]
 
-// const genres = [
-//   {
-//     id: 0,
-//     genre: 'Рок',
-//   },
-//   {
-//     id: 1,
-//     genre: 'Хип-хоп',
-//   },
-//   {
-//     id: 2,
-//     genre: 'Поп-музыка',
-//   },
-//   {
-//     id: 3,
-//     genre: 'Техно',
-//   },
-//   {
-//     id: 4,
-//     genre: 'Инди',
-//   },
-// ]
+const genres = [
+  {
+    id: 0,
+    genre: 'Рок',
+  },
+  {
+    id: 1,
+    genre: 'Хип-хоп',
+  },
+  {
+    id: 2,
+    genre: 'Поп-музыка',
+  },
+  {
+    id: 3,
+    genre: 'Техно',
+  },
+  {
+    id: 4,
+    genre: 'Инди',
+  },
+]
 
-// const years = [
-//   {
-//     id: 0,
-//     year: '2022',
-//   },
-//   {
-//     id: 1,
-//     year: '2021',
-//   },
-//   {
-//     id: 2,
-//     year: '2020',
-//   },
-//   {
-//     id: 3,
-//     year: '2019',
-//   },
-//   {
-//     id: 4,
-//     year: '2018',
-//   },
-// ]
+const years = [
+  {
+    id: 0,
+    year: '2022',
+  },
+  {
+    id: 1,
+    year: '2021',
+  },
+  {
+    id: 2,
+    year: '2020',
+  },
+  {
+    id: 3,
+    year: '2019',
+  },
+  {
+    id: 4,
+    year: '2018',
+  },
+]
 
-// function FilterPopup({ isPopupVisible, filterBy, coords }) {
-//   let content = ''
+function FilterPopup({ isPopupVisible, filterBy, coords }) {
+  let content = ''
 
-//   content = filterBy.map((item) => (
-//     <a href="#" className="dropdawn-item" key={item.id}>
-//       {item.year}
-//     </a>
-//   ))
+  switch (filterBy) {
+    case 'author':
+      content = authors.map((author) => (
+        <div className="dropdown-item" key={author.id}>
+          {author.author}
+        </div>
+      ))
+      break
+    case 'year':
+      content = years.map((year) => (
+        <div className="dropdown-item" key={year.id}>
+          {year.year}
+        </div>
+      ))
+      break
+    case 'genre':
+      content = genres.map((genre) => (
+        <div className="dropdown-item" key={genre.id}>
+          {genre.genre}
+        </div>
+      ))
+      break
+    default:
+      content = 'Нет данных'
+      break
+  }
 
-//   return isPopupVisible ? (
-//     <div className="dropdown-menu" style={{ top: coords.top, left: coords.left }}>
-//       {content}
-//     </div>
-//   ) : null
-// }
+  return isPopupVisible ? (
+    <div className="dropdown-menu" style={{ top: coords.top, left: coords.left }}>
+      {content}
+    </div>
+  ) : null
+}
 
-// export default FilterPopup
+export default FilterPopup
